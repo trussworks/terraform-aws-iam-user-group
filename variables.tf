326c71a0022d24f1ad39bc87583580fc2c431c05
Elizabeth Eady <eeeady@truss.works>
2019-09-13 16:17:49 -0700

    removing force_destroy

--- variables.tf
+++ variables.tf
@@ -1,13 +1,7 @@
-variable "user_names" {
+variable "user_list" {
   description = "List of IAM users to add to the group."
   type        = list(string)
   default     = []
-}
-
-variable "force_destroy_users" {
-  description = "Sets 'force_destroy' to true or false for all IAM users generated with this module. Note:Turning force_destroy off means that a user with IAM permissions will have to go and remove MFA and keys from a user you might be managing this way."
-  type        = bool
-  default     = true
 }
 
 variable "group_name" {
