15f9c9acd5d0fac5c230bdf0260d456b8caf0e24
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2020-01-21 15:46:51 -0600

    Gets rid of 0.11 Syntax error. Makes the module look a bit more like the others.

--- main.tf
+++ main.tf
@@ -7,8 +7,7 @@ resource "aws_iam_group" "user_group" {
 }
 
 resource "aws_iam_group_membership" "user_group" {
-  name = "${var.group_name}-membership"
-
+  name  = "${var.group_name}-membership"
   users = var.user_list
 
   group = aws_iam_group.user_group.name
@@ -36,3 +35,4 @@ resource "aws_iam_group_policy_attachment" "assume_role_policy_attachment" {
   group      = "${aws_iam_group.user_group.name}"
   policy_arn = "${aws_iam_policy.assume_role_policy.arn}"
 }
+
